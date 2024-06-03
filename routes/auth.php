ea76b331ae2cf1f40c4e699d25d89357e3429fbd
André Marques <andre.costa.marques@hotmail.com>
2024-06-03 08:20:23 -0300

    correção do rotas auth

--- routes/auth.php
+++ routes/auth.php
@@ -11,10 +11,7 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 
-    Route::get('register', [RegisteredUserController::class, 'create'])
-                ->name('register');
-
-    Route::post('register', [RegisteredUserController::class, 'store']);
+Route::middleware('guest')->group(function () {
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
